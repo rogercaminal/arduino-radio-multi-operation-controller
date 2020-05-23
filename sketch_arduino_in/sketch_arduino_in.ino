d2d1eae69022ec6d8a457455661950a3461ed567
rogercaminal <roger.caminal@gmail.com>
2020-05-24 01:54:25 +0200

    Updates on the sketches

--- sketch_arduino_in/sketch_arduino_in.ino
+++ sketch_arduino_in/sketch_arduino_in.ino
@@ -1,57 +1,22 @@
 #include <SoftwareSerial.h>
+#define N_RIGS 6
+#define N_BANDS 6
 
 //=====[ Settings ]===========================================================================================
 int PIN_SERIAL_RX = 2;
 int PIN_SERIAL_TX = 3;
 
-int PIN_R1_10 = 10
-int PIN_R1_15 = 11
-int PIN_R1_20 = 12
-int PIN_R1_40 = 13
-int PIN_R1_80 = 14
-int PIN_R1_160 = 15
+//                                    10m 15m 20m 40m 80m 160m  
+int PIN_RIG_BAND[N_RIGS][N_BANDS] = {{10, 11, 12, 13, 14, 15},   // RIG1 
+                                     {16, 17, 18, 19, 20, 21},   // RIG2
+                                     {22, 23, 24, 25, 26, 27},   // RIG3
+                                     {28, 29, 30, 31, 32, 33},   // RIG4
+                                     {34, 35, 36, 37, 38, 39},   // RIG5
+                                     {40, 41, 42, 43, 44, 45}};  // RIG6
 
-int PIN_R2_10 = 16
-int PIN_R2_15 = 17
-int PIN_R2_20 = 18
-int PIN_R2_40 = 19
-int PIN_R2_80 = 20
-int PIN_R2_160 = 21
+int PIN_BUZZ = 4;
 
-int PIN_R3_10 = 22
-int PIN_R3_15 = 23
-int PIN_R3_20 = 24
-int PIN_R3_40 = 25
-int PIN_R3_80 = 26
-int PIN_R3_160 = 27
-
-int PIN_R4_10 = 28
-int PIN_R4_15 = 29
-int PIN_R4_20 = 30
-int PIN_R4_40 = 31
-int PIN_R4_80 = 32
-int PIN_R4_160 = 33
-
-int PIN_R5_10 = 34
-int PIN_R5_15 = 35
-int PIN_R5_20 = 36
-int PIN_R5_40 = 37
-int PIN_R5_80 = 38
-int PIN_R5_160 = 39
-
-int PIN_R6_10 = 40
-int PIN_R6_15 = 41
-int PIN_R6_20 = 42
-int PIN_R6_40 = 43
-int PIN_R6_80 = 44
-int PIN_R6_160 = 45
-
-int PIN_BUZZ = 4
-
-int N_RIGS = 6
-int N_BANDS = 6
-
-int BAUD_RATE = 38400
+int BAUD_RATE = 38400;
 
 // Define the serial pin object
 SoftwareSerial board_serial(PIN_SERIAL_RX, PIN_SERIAL_TX);
@@ -64,39 +29,32 @@ int states[N_RIGS][N_BANDS];
 void update_states_from_rigs(){
   // This function reads the different pins and updates the states
   // matrix.
-  int a = 1;
+  for (int i = 0; i < N_BANDS; ++i) { 
+    for (int j = 0; j < N_RIGS; ++j) {
+      if (digitalRead(PIN_RIG_BAND[i][j]) == HIGH){
+        states[i][j] = 1;
+      }
+      else{
+        states[i][j] = 0;
+      }
+    }
+  }
 }
 
 
 bool check_states(){
   // This function makes sure that there is only one rig per band. To do this
-  // it computes the sum of the elements in each column and row and verifies 
+  // it computes the sum of the elements in each column and verifies 
   // that it is always equal or smaller than 1.
   // If this is the case it returns true, otherwise false.
-  int sum = 0;
   bool is_fine = true;
-  
-  // finding the row sum 
-  sum = 0;
-  for (i = 0; i < N_RIGS; ++i) { 
-    for (j = 0; j < N_BANDS; ++j) {   
-      // Add the element 
-      sum = sum + states[i][j];
-    } 
-    // Check if it is larger than 1
-    if (sum > 1) {
-      is_fine = false;  
-    }
-    // Reset the sum 
-    sum = 0; 
-  }
 
-  // finding the column sum 
-  sum = 0;
-  for (i = 0; i < N_BANDS; ++i) { 
-    for (j = 0; j < N_RIGS; ++j) {   
+  // Not more than one rig on the same band
+  int sum = 0;
+  for (int i = 0; i < N_BANDS; ++i) { 
+    for (int j = 0; j < N_RIGS; ++j) {   
       // Add the element 
-      sum = sum + states[j][i];
+      sum += states[j][i];
     } 
     // Check if it is larger than 1
     if (sum > 1) {
@@ -113,7 +71,19 @@ bool check_states(){
 void send_states_through_serial(){
   // This function sends the states through the serial port for
   // the out arduino to read them and take action
-  int a = 1;
+
+  // Transform the states matrix to the array to be sent
+  byte transmitted_state[N_RIGS*N_BANDS];
+  for (int i = 0; i < N_RIGS; i++) {
+    for (int j = 0; j < N_BANDS; j++) {
+      transmitted_state[N_RIGS*i + N_BANDS] = byte(states[i][j]);
+    }
+  }
+  
+  // Send it through the serial
+  if (board_serial.available()) {
+    board_serial.write(transmitted_state, N_RIGS*N_BANDS);
+  }
 }
 
 
@@ -125,62 +95,35 @@ void setup() {
   board_serial.begin(BAUD_RATE);
   board_serial.println("Setting up...");
   
-  // Initialize states
-  for (int i =0; i < N_RIGS; i++) {
-    for (int j =0; j < N_BANDS; j++) {
+  // Initialize states and setup input pins
+  for (int i = 0; i < N_RIGS; i++) {
+    for (int j = 0; j < N_BANDS; j++) {
       states[i][j] = 0;
+      pinMode(PIN_RIG_BAND[i][j], INPUT);
     }
   }
 
-  // Setup pins
-  pinMode(PIN_R1_10, INPUT);
-  pinMode(PIN_R1_15, INPUT);
-  pinMode(PIN_R1_20, INPUT);
-  pinMode(PIN_R1_40, INPUT);
-  pinMode(PIN_R1_80, INPUT);
-  pinMode(PIN_R1_160, INPUT);
-  
-  pinMode(PIN_R2_10, INPUT);
-  pinMode(PIN_R2_15, INPUT);
-  pinMode(PIN_R2_20, INPUT);
-  pinMode(PIN_R2_40, INPUT);
-  pinMode(PIN_R2_80, INPUT);
-  pinMode(PIN_R2_160, INPUT);
-  
-  pinMode(PIN_R3_10, INPUT);
-  pinMode(PIN_R3_15, INPUT);
-  pinMode(PIN_R3_20, INPUT);
-  pinMode(PIN_R3_40, INPUT);
-  pinMode(PIN_R3_80, INPUT);
-  pinMode(PIN_R3_160, INPUT);
-
-  pinMode(PIN_R4_10, INPUT);
-  pinMode(PIN_R4_15, INPUT);
-  pinMode(PIN_R4_20, INPUT);
-  pinMode(PIN_R4_40, INPUT);
-  pinMode(PIN_R4_80, INPUT);
-  pinMode(PIN_R4_160, INPUT);
-
-  pinMode(PIN_R5_10, INPUT);
-  pinMode(PIN_R5_15, INPUT);
-  pinMode(PIN_R5_20, INPUT);
-  pinMode(PIN_R5_40, INPUT);
-  pinMode(PIN_R5_80, INPUT);
-  pinMode(PIN_R5_160, INPUT);
-
-  pinMode(PIN_R6_10, INPUT);
-  pinMode(PIN_R6_15, INPUT);
-  pinMode(PIN_R6_20, INPUT);
-  pinMode(PIN_R6_40, INPUT);
-  pinMode(PIN_R6_80, INPUT);
-  pinMode(PIN_R6_160, INPUT);
+  // Setup output pin
+  pinMode(PIN_BUZZ, OUTPUT);
 }
 
 // Loop function
 void loop() {
-  // 1. Check radio states
-  // 2. Check if there's only one rig per band
-  // 3. If not, make the buzzer sound
-  // 4. If so, send the state to the output board
+  // Check radio states
+  update_states_from_rigs();
+  
+  // Check if there's only one rig per band
+  bool ok = check_states();
 
+  // If it is OK, send the states to the out arduino and make sure the 
+  // buzzer is off
+  if (ok == true){
+    send_states_through_serial();
+    digitalWrite(PIN_BUZZ, LOW);
+  }
+  // If it is not OK, make the buzzer sound (and do not pass any state update
+  // out arduino
+  else{
+    digitalWrite(PIN_BUZZ, HIGH);
+  }
 }
--- sketch_arduino_out/sketch_arduino_out.ino
+++ sketch_arduino_out/sketch_arduino_out.ino
@@ -1,9 +1,86 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
+#define N_RIGS 6
+#define N_BANDS 6
 
+//=====[ Settings ]===========================================================================================
+int PIN_SERIAL_RX = 2;
+int PIN_SERIAL_TX = 3;
+
+//                                    10m 15m 20m 40m 80m 160m  
+int PIN_RIG_BAND[N_RIGS][N_BANDS] = {{10, 11, 12, 13, 14, 15},   // RIG1 
+                                     {16, 17, 18, 19, 20, 21},   // RIG2
+                                     {22, 23, 24, 25, 26, 27},   // RIG3
+                                     {28, 29, 30, 31, 32, 33},   // RIG4
+                                     {34, 35, 36, 37, 38, 39},   // RIG5
+                                     {40, 41, 42, 43, 44, 45}};  // RIG6
+
+int BAUD_RATE = 38400;
+
+// Define the serial pin object
+SoftwareSerial board_serial(PIN_SERIAL_RX, PIN_SERIAL_TX);
+
+// Define matrix of state. Rows are rigs, columns are bands.
+int states[N_RIGS][N_BANDS];
+
+
+//=====[ Aux functions ]===========================================================================================
+void update_output_from_states(){
+  // This function reads the different pins and updates the states
+  // matrix.
+  for (int i = 0; i < N_BANDS; ++i) { 
+    for (int j = 0; j < N_RIGS; ++j) {
+      if (states[i][j] == 1){
+        digitalWrite(PIN_RIG_BAND[i][j], HIGH);
+      }
+      else{
+        digitalWrite(PIN_RIG_BAND[i][j], LOW);
+      }
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void get_states_from_serial(){
+  // This function receives the states from the serial port
+
+/*
+  // Transform the states matrix to the array to be sent
+  byte transmitted_state[N_RIGS*N_BANDS];
+  for (int i = 0; i < N_RIGS; i++) {
+    for (int j = 0; j < N_BANDS; j++) {
+      transmitted_state[N_RIGS*i + N_BANDS] = byte(states[i][j]);
+    }
+  }
+  
+  // Send it through the serial
+  if (board_serial.available()) {
+    board_serial.write(transmitted_state, N_RIGS*N_BANDS);
+  }
+  */
+}
+
+
+//=====[ Main functions ]===========================================================================================
+// Setup function
+void setup() {
+
+  // Set the data rate for the SoftwareSerial port
+  board_serial.begin(BAUD_RATE);
+  board_serial.println("Setting up...");
+  
+  // Initialize states and setup input pins
+  for (int i = 0; i < N_RIGS; i++) {
+    for (int j = 0; j < N_BANDS; j++) {
+      states[i][j] = 0;
+      pinMode(PIN_RIG_BAND[i][j], OUTPUT);
+    }
+  }
+}
+
+// Loop function
+void loop() {
+  // Read the serial
+  
+  // Update the output states
+  update_output_from_states();
 }
